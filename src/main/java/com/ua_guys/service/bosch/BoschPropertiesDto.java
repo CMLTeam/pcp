b973dce21cda2b1cb97a09849d4fd15f43570b8d
v_tomashchuk <tomashchuk1809@gmail.com>
2020-02-16 06:27:46 +0200

    Last seen timestamp

--- src/main/java/com/ua_guys/service/bosch/BoschPropertiesDto.java
+++
@@ -1,4 +0,0 @@
-package com.ua_guys.service.bosch;
-
-public class BoschPropertiesDto {
-}
---
+++ src/main/java/com/ua_guys/service/bosch/LastSeenValueDto.java
@@ -0,0 +1,12 @@
+package com.ua_guys.service.bosch;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class LastSeenValueDto {
+
+    private final Date value;
+
+}
