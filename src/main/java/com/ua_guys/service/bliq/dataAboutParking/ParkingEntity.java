d8dd84008819d8a1b58a18e6296b3da4e04c9cff
evRusin <evrusin@gmail.com>
2020-02-16 06:52:01 +0200

    dto finished

--- src/main/java/com/ua_guys/service/bliq/dataAboutParking/ParkingEntity.java
+++ src/main/java/com/ua_guys/service/bliq/dataAboutParking/ParkingEntity.java
@@ -6,20 +6,69 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @JsonSerialize
 @Getter
 @Setter
 @ToString
 public class ParkingEntity {
   private Integer capacity;
-  private OccupancyPredicted occupancyPredicted;
+  private Occupancy occupancy;
   private PriceInformation priceInformation;
 }
 
+
+@JsonSerialize
+@Getter
+@Setter
+@ToString
+class Occupancy {
+  private OccupancyPredicted occupancyPredicted;
+}
+
+
+@JsonSerialize
+@Getter
+@Setter
+@ToString
 class OccupancyPredicted {
   private Double parkingProbability;
 }
 
-class PriceInformation {
 
+@JsonSerialize
+@Getter
+@Setter
+@ToString
+class PriceInformation {
+  private List<Schedule> schedules;
+}
+
+
+@JsonSerialize
+@Getter
+@Setter
+@ToString
+class Schedule {
+  private PriceModel priceModel;
+}
+
+
+@JsonSerialize
+@Getter
+@Setter
+@ToString
+class PriceModel {
+  private List<Price> prices;
+}
+
+
+@JsonSerialize
+@Getter
+@Setter
+@ToString
+class Price {
+  private String currencyCode;
+  private String priceValue;
 }
