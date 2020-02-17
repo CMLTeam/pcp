e7ca556fc824e315b87d30a348ebf6a7741656ac
Kopylov Oleksii <tonn.tamerlan@gmail.com>
2020-02-17 14:21:44 +0200

    fix

--- src/main/java/com/ua_guys/dto/Parking.java
+++ src/main/java/com/ua_guys/dto/Parking.java
@@ -7,40 +7,44 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @Data
 @Builder
 public class Parking {
-  private Integer capacity;
-  private Integer probability;
-  private Integer available;
-  private BigDecimal price;
   private Type type;
   private Point geometry;
+  private Map<String, Object> properties;
 
   public static Parking of(ParkingFeture parkingFeture) {
+    Map<String, Object> properties = new HashMap<>();
+    properties.put("capacity", parkingFeture.getProperties().getParkingEntity().getCapacity());
+    properties.put(
+        "probability",
+        parkingFeture
+            .getProperties()
+            .getParkingEntity()
+            .getOccupancy()
+            .getOccupancyPredicted()
+            .getParkingProbability());
+    properties.put(
+        "available",
+        parkingFeture
+                .getProperties()
+                .getParkingEntity()
+                .getOccupancy()
+                .getOccupancyPredicted()
+                .getParkingProbability()
+            * parkingFeture.getProperties().getParkingEntity().getCapacity()
+            / 100);
+    properties.put("price", BigDecimal.valueOf(new Random().nextFloat() * 2));
+
     Parking parking =
         Parking.builder()
-            .capacity(parkingFeture.getProperties().getParkingEntity().getCapacity())
-            .probability(
-                parkingFeture
-                    .getProperties()
-                    .getParkingEntity()
-                    .getOccupancy()
-                    .getOccupancyPredicted()
-                    .getParkingProbability())
-            .available(
-                parkingFeture
-                        .getProperties()
-                        .getParkingEntity()
-                        .getOccupancy()
-                        .getOccupancyPredicted()
-                        .getParkingProbability()
-                    * parkingFeture.getProperties().getParkingEntity().getCapacity()
-                    / 100)
-            .price(BigDecimal.valueOf(new Random().nextFloat() * 2))
             .type(Type.Feature)
+            .properties(properties)
             .geometry(new Point(Type.Point, parkingFeture.getGeometry().getCoordinates().get(0)))
             .build();
 
